7b59d8a2cea6fc2723f7dbb9b8133171a7458e2a
nabin <nkbhaie2017@gmail.com>
2022-01-21 22:46:54 -0700

    add some functions for trees.js

--- dsa-trees/tree.js
+++ dsa-trees/tree.js
@@ -15,19 +15,74 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    //   let node = new TreeNode();
+    //   let sum = 0;
+    if (!this.root){
+        return 0;
+    }
+    let sum = this.root.val;
+    // get helper function here so that it helps to use TreeNode
+    function helper(node){
+      for(let item of node.children){
+          // get all the values
+          sum += item.val;
+          //if a child has its child
+          if(child.children.length > 0){
+              // do a recursive func call
+              helper(item)
+          }
+      }
+    }
+    helper(this.root);
+    return sum;
+    // instantiate TreeNode
+    //   const node = new TreeNode()
     
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    if (!this.root){
+        return 0;
+    }
+    let count = this.root.val % 2 === 0 ? 1 : 0;
 
+    function helper(node){
+        for(let child of node.children){
+            if(child.val % 2 === 0){
+                count += count;
+            }
+            if(child.children.length > 0){
+                helper(child);
+            }
+        }
+    }
+    helper(this.root);
+    return count;
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+      if(!this.root){
+          return 0;
+      }
+      let count = this.root.val > lowerBound ? 1 : 0;
+
+      function helper(node){
+          for(let child of node.children){
+              if(child.val > lowerBound){
+                  count += count;
+              }
+              if(child.children.length > 0){
+                  helper(child);
+              }
+          }
+      }
+      helper(this.root);
+      return count;
 
   }
 }
