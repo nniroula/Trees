98f649a231e5c384ceac5bb42a643062d35128ca
nabin <nkbhaie2017@gmail.com>
2022-01-21 22:46:10 -0700

    work on some functions

--- dsa-trees/binary-tree.js
+++ dsa-trees/binary-tree.js
@@ -17,6 +17,24 @@ class BinaryTree {
    * the length of the shortest path from the root to a leaf. */
 
   minDepth() {
+    //   const binaryNode = new BinaryTree();
+    if (!this.root){
+        return 0;
+    }
+    // create a helper function to use BinaryTreeNode
+    function helper(node){
+        if(node.left === null && node.right === null){
+            return 1;
+        }
+        if(node.left === null){
+            return helper(node.right) + 1;
+        }
+        if(node.right === null){
+            return helper(node.left) + 1;
+        }
+        return Math.min(helper(node.left), helper(node.right) + 1);
+    }
+    return helper(this.root);
 
   }
 
@@ -24,6 +42,23 @@ class BinaryTree {
    * the length of the longest path from the root to a leaf. */
 
   maxDepth() {
+      if (!this.root){
+          return 0;
+      }
+
+      function helper(node){
+          if(node.left === null && node.right === null){
+              return 1;
+          }
+          if(node.left === null){
+              return helper(node.right) + 1;
+          }
+          if(node.right === null){
+              return helper(node.left) + 1;
+          }
+          return Math.max(helper(node.left), helper(node.right)) + 1;
+      }
+      return helper(this.root);
 
   }
 
@@ -31,6 +66,19 @@ class BinaryTree {
    * The path doesn't need to start at the root, but you can't visit a node more than once. */
 
   maxSum() {
+      let result = 0;
+
+      function helper(node){
+          if(node === null){
+              return 0;
+          }
+          let left_sum = helper(node.left);
+          let right_sum = helper(node.right);
+          result = Math.max(result, node.val + left_sum + right_sum);
+          return Math.max(0, left_sum + node.val, right_sum + node.val);
+      }
+      helper(this.root);
+      return result;
 
   }
 
@@ -53,6 +101,20 @@ class BinaryTree {
    * serialize(tree): serialize the BinaryTree object tree into a string. */
 
   static serialize() {
+      let val = [];
+
+      function traverse(node){
+          if(node){
+              val.push(node.val);
+              traverse(node.left);
+              traverse(node.right);
+          }
+          else{
+              val.push("#");
+          }
+      }
+      traverse(tree.root);
+      return val.join(" ");
 
   }
 
